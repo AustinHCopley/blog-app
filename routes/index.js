17502856d5ddcd78baf9a071ff092551630e37da
AustinHCopley <acopley@mines.edu>
2022-07-13 14:33:26 -0600

    small changes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
 
 export default express.Router().get("/", function (req, res, next) {
-  res.render("index", { title: "My Website" });
+  res.render("index", { title: "Blog Site" });
 });
